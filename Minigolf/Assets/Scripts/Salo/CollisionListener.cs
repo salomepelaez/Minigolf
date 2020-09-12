9e940a5d19fe2b240e9f7fa06719b9bacc7844a9
Salomé Pelaez <spsalome@gmail.com>
2020-09-11 21:50:51 -0500

    Colisiones

---
+++ Minigolf/Assets/Scripts/Salo/CollisionListener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollisionListener : MonoBehaviour
+{
+    public Action<Collision> onCollisionEnter = null;
+    public Action<Collision> onCollisionState = null;
+    public Action<Collision> onCollisonExit = null;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(onCollisionEnter != null)
+        {
+            onCollisionEnter(collision);
+        }
+    }
+}
---
+++ Minigolf/Assets/Scripts/Salo/DragListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*public class DragListener : MonoBehaviour, IDragHandler, IBeginDrag, IEndDrag
+{
+    public Action<PointerEventData> onBeginDrag = null;
+    public Action<PointerEventData> onDrag = null;
+    public Action<PointerEventData> onEndDrag = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+}*/
