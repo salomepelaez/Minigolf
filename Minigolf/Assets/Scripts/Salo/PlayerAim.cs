6b853510eb30acc665cd6a6d0b340e91f195dff6
Salomé Pelaez <spsalome@gmail.com>
2020-09-11 19:15:40 -0500

    Primer commit
    Prefabs, skybox y Player aim

---
+++ Minigolf/Assets/Scripts/Salo/PlayerAim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+
+    float sensitivity = 40.0f;
+
+    public Transform target, player;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamMove();
+    }
+
+    void CamMove()
+    {
+        mouseX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Debug.Log("si");
+        }
+
+        else
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+}
