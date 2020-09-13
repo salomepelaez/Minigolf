80cc65bd8d0e3a8d688268647f977cd061de7e9c
Salomé Pelaez <spsalome@gmail.com>
2020-09-12 19:48:43 -0500

    Pelota de golf

--- Minigolf/Assets/Scripts/Carlos/DragListener.cs
+++ Minigolf/Assets/Scripts/Carlos/DragListener.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragListener : MonoBehaviour
+public class DragListener : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public Action<PointerEventData> onBeginDrag     = null;
     public Action<PointerEventData> onDrag          = null;
@@ -10,14 +10,25 @@ public class DragListener : MonoBehaviour
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-
+        if(onBeginDrag != null)
+        {
+            onBeginDrag(eventData);
+        }
     }
+
     public void OnDrag(PointerEventData eventData)
     {
-
+        if(onDrag != null)
+        {
+            onDrag(eventData);
+        }
     }
+
     public void OnEndDrag(PointerEventData eventData)
     {
-
+        if(onEndDrag != null)
+        {
+            onEndDrag(eventData);
+        }
     }
 }
