6a2114c30cf52b68d6b164d26416a85273170aea
Salomé Pelaez <spsalome@gmail.com>
2020-09-12 23:12:10 -0500

    Niveles y UI

---
+++ Minigolf/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene("PrimerNivel");
+    }
+
+    public void StartSecondLevel()
+    {
+        SceneManager.LoadScene("SegundoNivel");
+    }
+
+    public void StartThirdLevel()
+    {
+        SceneManager.LoadScene("TercerNivel");
+    }
+
+    public void StartForthLevel()
+    {
+        SceneManager.LoadScene("CuartoNivel");
+    }
+
+    public void StartFifthLevel()
+    {
+        SceneManager.LoadScene("QuintoNivel");
+    }
+
+    public void StartSixthLevel()
+    {
+        SceneManager.LoadScene("SextoNivel");
+    }
+    
+}
